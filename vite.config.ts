868b678ae4f37e3f6ddff4b502de44e4402b3706
PacificD <pacificd@usemotionvalue.local>
2023-11-14 14:37:08 +0800

    chore: dev https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    https: true
+  },
+  plugins: [react(), mkcert()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
