2895d3ad16bcc498c5c0b5788cac9d8a3ad514b9
PacificD <pacificd@usemotionvalue.local>
2023-11-14 17:52:48 +0800

    chore: webrtc-adapter

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import 'webrtc-adapter'
+
 import RouteElement from './routes'
 import './index.css'
 
