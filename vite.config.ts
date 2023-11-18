5f0124940935582aab78fdff57c44ee0dc72afbb
PacificD <1484636442@qq.com>
2023-11-18 11:44:59 +0800

    chore: vite-plugin-basic-ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
-import mkcert from 'vite-plugin-mkcert'
+import basicSSL from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: true
-  },
-  plugins: [react(), mkcert()],
+
+  plugins: [react(), basicSSL()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
